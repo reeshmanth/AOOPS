a086673239c971e72fee5c60cd55b651f7382c09
reeshmanth <156885754+reeshmanth@users.noreply.github.com>
2025-01-07 23:10:13 +0530

    Add files via upload

---
+++ TASK4_2/src/TASK4_2/JSONImporter.java
@@ -0,0 +1,21 @@
+package TASK4_2;
+
+class JSONImporter extends DataImporter {
+    @Override
+    protected void readData(String filePath) {
+        System.out.println("Reading JSON data from " + filePath);
+        
+    }
+
+    @Override
+    protected void parseData() {
+        System.out.println("Parsing JSON data...");
+        
+    }
+
+    @Override
+    protected void validateData() {
+        System.out.println("Validating JSON data...");
+        
+    }
+}
