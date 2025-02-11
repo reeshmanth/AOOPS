e3269b42333a2ef473c51b071f874d825e82c5d4
reeshmanth <156885754+reeshmanth@users.noreply.github.com>
2025-02-11 08:58:50 +0530

    Add files via upload

---
+++ TASK7_2/src/InLab2/DepartmentComparator.java
@@ -0,0 +1,9 @@
+package InLab2;
+import java.util.Comparator;
+
+class DepartmentComparator implements Comparator<Employee> {
+    @Override
+    public int compare(Employee e1, Employee e2) {
+        return e1.getDepartment().compareTo(e2.getDepartment());
+    }
+}
---
+++ TASK7_2/src/InLab2/SalaryAscComparator.java
@@ -0,0 +1,8 @@
+package InLab2;
+import java.util.Comparator;
+class SalaryAscComparator implements Comparator<Employee> {
+    @Override
+    public int compare(Employee e1, Employee e2) {
+        return Double.compare(e1.getSalary(), e2.getSalary());
+    }
+}
